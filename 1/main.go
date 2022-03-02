7d687599ce389e2f31ec3b0397e3fe66068ecb4b
Soung Bae Kim <sbkim@berkeley.edu>
2022-03-02 00:00:39 -0800

    added more lines of prints in 1

--- 1/main.go
+++ 1/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	name := "isaac"
+	other := "brian"
+
+	fmt.Printf("Hello, %s and %s", name, other)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("Hello")
+	}
 }
