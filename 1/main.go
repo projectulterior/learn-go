5e7fe3e9240cb1e03960e10e162f994cd1d45b34
Justin Oh <justinys.oh17@gmail.com>
2022-03-03 22:57:49 -0800

    change names

--- 1/main.go
+++ 1/main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 
-	name := "isaac"
-	other := "brian"
+	name := "justin"
+	other := "ur mom"
 
 	fmt.Printf("Hello, %s and %s", name, other)
 
