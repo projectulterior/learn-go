7acd2afd903b8fcdabad5f0f6975924cd1871126
Soung Bae Kim <sbkim@berkeley.edu>
2022-03-06 23:02:00 -0800

    google search

---
+++ 2/go.mod
@@ -0,0 +1,3 @@
+module github.com/projectulterior/learn-go/2
+
+go 1.15
--- 2/main.go
+++ 2/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		request, _ := http.NewRequest(http.MethodGet, "https://www.google.com/search?q=calebpark", nil)
+		resp, _ := http.DefaultClient.Do(request)
+
+		response, _ := ioutil.ReadAll(resp.Body)
+		fmt.Fprintf(w, string(response))
 		fmt.Fprintf(w, "Hello, Caleb!")
 	})
 
